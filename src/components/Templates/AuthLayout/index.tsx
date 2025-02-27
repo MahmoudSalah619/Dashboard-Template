8149f6a21d2196045f2cce1db9753e4e5e1a5f39
chickenleader <mahmoud.s.m619@gmail.com>
2025-02-27 17:04:53 +0200

    adjusted logo

--- src/components/Templates/AuthLayout/index.tsx
+++ src/components/Templates/AuthLayout/index.tsx
@@ -3,6 +3,7 @@ import { ReactNode } from "react";
 import styles from "./styles.module.scss";
 import Logo from "@/src/assets/images/Logo.png";
 import Image from "../../Atoms/Image";
+import Text from "../../Atoms/Text";
 
 function AuthLayout({
   children,
@@ -16,7 +17,8 @@ function AuthLayout({
       style={{ backgroundColor: "#fff", height: "100%", minHeight: "100vh" }}
     >
       <div className={`${styles.container} ${styles[spaceTop]}`}>
-        <Image src={Logo} alt="Logo" width={292} height={170} />
+        {/* <Image src={Logo} alt="Logo" width={292} height={170} /> */}
+        <Text fontSize={150}>Logo</Text>
         {children}
       </div>
     </Layout>
